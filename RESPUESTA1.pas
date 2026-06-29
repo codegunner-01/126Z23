79c7dbdd3a2416a266685b7cf2d49075cba86b98
codegunner-01 <codegunner2025@gmail.com>
2026-06-28 22:01:29 -0400

    Create RESPUESTA1.pas

---
+++ RESPUESTA1.pas
@@ -0,0 +1,20 @@
+
+// Copiar el ejercicio 1 debajo del comentario "{ EJERCICIO 1 COPIAR ABAJO }"
+
+
+//EJERCICIO 1
+
+
+
+
+
+
+
+
+
+
+
+//==========================================================================================
+// Copiar la llamada del ejercicio 1 debajo del comentario "{ EJERCICIO 1 COPIAR ABAJO }"
+
+//LLAMADA DEL EJERCICIO 1
