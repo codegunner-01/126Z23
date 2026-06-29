aeec310c73ea0c02ac2087b7ffc53606b15fb138
codegunner-01 <codegunner2025@gmail.com>
2026-06-29 11:23:30 -0400

    Update RESPUESTA4.pas

--- RESPUESTA4.pas
+++ RESPUESTA4.pas
@@ -1,1 +1,30 @@
 //EJERCICIO 4
+    // COPIAR AQUI LA CABECERA
+    function intercambiarPrimerUltimo: Integer;
+
+
+    
+// COPIAR AQUI EL EJERCICIO POO
+function NumeroNatural.intercambiarPrimerUltimo: Integer;
+var
+  p, pri, ult, cen: Word;
+begin
+  if Valor < 10 then
+    intercambiarPrimerUltimo := Valor
+  else
+  begin
+    p := 1;
+    while (Valor div (p * 10)) > 0 do
+      p := p * 10;
+
+    pri := Valor div p;
+    ult := Valor mod 10;
+    cen := (Valor mod p) div 10;
+
+    intercambiarPrimerUltimo := ult * p + cen * 10 + pri;
+  end;
+end;
+
+  // COPIAR AQUI LA LLAMADA
+
+       Edit2.Text := IntToStr(Numero.intercambiarPrimerUltimo);
