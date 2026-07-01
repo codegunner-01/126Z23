3c23e93974d050fc5903bdd54bcc13fcafbdec2a
codegunner-01 <codegunner2025@gmail.com>
2026-07-01 08:46:08 -0400

    Update RESPUESTA2.pas

--- RESPUESTA2.pas
+++ RESPUESTA2.pas
@@ -2,44 +2,13 @@
 
 
 //EJERCICIO 2
-function ejercicio2(n: Integer): Real;
-var
-  i, j: Integer;
-  sumaNum, frac, suma: Real;
-  sw: Boolean;
-begin
-  suma := 0;
-  sw := True;
 
-  for i := 1 to n do
-  begin
-    sumaNum := 0;
-
-    for j := 1 to i do
-      sumaNum := sumaNum + j;
-
-    frac := sumaNum / i;
-
-    if sw then
-    begin
-      suma := suma + frac;
-      sw := False;
-    end
-    else
-    begin
-      suma := suma - frac;
-      sw := True;
-    end;
-  end;
-
-  Result := suma;
-end;
 
 
 //==========================================================================================
 // Copiar la llamada del ejercicio 2 debajo del comentario "{ EJERCICIO 2 COPIAR ABAJO }"
 
 //LLAMADA DEL EJERCICIO 2
-    Edit3.Text := FloatToStr(ejercicio2(n));
+  
 
     
