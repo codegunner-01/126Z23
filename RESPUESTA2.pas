7a624be07864b7061d32014b97b94f76363f1fca
codegunner-01 <codegunner2025@gmail.com>
2026-07-01 09:02:58 -0400

    Update RESPUESTA2.pas

--- RESPUESTA2.pas
+++ RESPUESTA2.pas
@@ -2,6 +2,50 @@
 
 
 //EJERCICIO 2
+function fct(n: Integer): Integer;
+var
+  i, f: Integer;
+begin
+  i := 0;
+  f := 1;
+
+  while i < n do
+  begin
+    i := i + 1;
+    f := f * i;
+  end;
+
+  Result := f;
+end;
+
+function ejercicio2(n: Integer): Real;
+var
+  i, num: Integer;
+  frac, suma: Real;
+  sw: Boolean;
+begin
+  suma := 0;
+  sw := True;
+
+  for i := 1 to n do
+  begin
+    num := i * i;
+    frac := num / fct(i);
+
+    if sw then
+    begin
+      suma := suma - frac;
+      sw := False;
+    end
+    else
+    begin
+      suma := suma + frac;
+      sw := True;
+    end;
+  end;
+
+  Result := suma;
+end;
 
 
 
@@ -9,6 +53,6 @@
 // Copiar la llamada del ejercicio 2 debajo del comentario "{ EJERCICIO 2 COPIAR ABAJO }"
 
 //LLAMADA DEL EJERCICIO 2
-  
+   Edit3.Text := FloatToStr(ejercicio2(n));
 
     
