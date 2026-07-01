139ff5560dc1ee16c9a6a810ce63ac3688f8f7e8
codegunner-01 <codegunner2025@gmail.com>
2026-07-01 08:46:18 -0400

    Update RESPUESTA3.pas

--- RESPUESTA3.pas
+++ RESPUESTA3.pas
@@ -1,52 +1,6 @@
 //EJERCICO 3
-function esCapicua(pal: string): Boolean;
-var
-  i, j: Integer;
-begin
-  i := 1;
-  j := Length(pal);
 
-  while i < j do
-  begin
-    if LowerCase(pal[i]) <> LowerCase(pal[j]) then
-    begin
-      Result := False;
-      Exit;
-    end;
-    Inc(i);
-    Dec(j);
-  end;
-
-  Result := True;
-end;
-
-function ejercicio3_cad(cad: string): string;
-var
-  i: Integer;
-  pal, res: string;
-begin
-  pal := '';
-  res := '';
-  cad := cad + ' ';
-
-  for i := 1 to Length(cad) do
-  begin
-    if cad[i] <> ' ' then
-      pal := pal + cad[i]
-    else
-    begin
-      if pal <> '' then
-      begin
-        if (Length(pal) > 1) and esCapicua(pal) then
-          res := res + pal + ' ';
-        pal := '';
-      end;
-    end;
-  end;
-
-  Result := res;
-end;
 
 
   //LLAMADA DE CADENAS AQUI
-  Edit3.Text := ejercicio3_cad(cad);
+
