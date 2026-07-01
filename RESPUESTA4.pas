e64447004c30bc10506b4a5e1ed16f0e582c5671
codegunner-01 <codegunner2025@gmail.com>
2026-07-01 09:41:04 -0400

    Update RESPUESTA4.pas

--- RESPUESTA4.pas
+++ RESPUESTA4.pas
@@ -1,12 +1,35 @@
 //EJERCICIO 4
     // COPIAR AQUI LA CABECERA
  
-
+    function DigitosAscendentes: Boolean;
 
     
 // COPIAR AQUI EL EJERCICIO POO
 
+function NumeroNatural.DigitosAscendentes: Boolean;
+var
+  n, d1, d2: Integer;
+begin
+  n := Valor;
+  DigitosAscendentes := True;
 
+  while n >= 10 do
+  begin
+    d1 := n mod 10;
+    d2 := (n div 10) mod 10;
+
+    if d2 > d1 then
+    begin
+      DigitosAscendentes := False;
+      Exit;
+    end;
+
+    n := n div 10;
+  end;
+end;
   // COPIAR AQUI LA LLAMADA
-
      
+  if Numero.DigitosAscendentes then
+    showmessage('Si')
+  else
+    showmessage('No');
